4525f2777b71f75b53c5b7530836ea4604bc7ae8
mhcomp <60882947+mhcomp@users.noreply.github.com>
2020-03-17 17:26:43 +0100

    Update graphNodes.repository.ts

--- src/graphNodes.repository.ts
+++ src/graphNodes.repository.ts
@@ -4,14 +4,10 @@ import { GraphNode } from "./graphNode.entity";
 
 @EntityRepository(GraphNode)
 export class GraphNodesRepository extends Repository<GraphNode> {
-  public createGraphNode(
-    graphId: string,
-    successorGraphNodeId?: string
-  ): Promise<GraphNode> {
+  public createGraphNode(graphId: string, successorGraphNodeId?: string): Promise<GraphNode> {
     const graphNode: GraphNode = new GraphNode();
     graphNode.graphId = graphId;
     graphNode.successorGraphNodeId = successorGraphNodeId;
-
     return graphNode.save();
   }
 }
